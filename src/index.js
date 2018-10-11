2d1e14ef2c62ac28d0596103329ea7e09e1e38e6
Anthony Langford <anthony.langford@gmail.com>
2018-10-11 13:32:39 -0400

    use styled components, minor formatting

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 ), document.getElementById('root'));
 
 registerServiceWorker();
